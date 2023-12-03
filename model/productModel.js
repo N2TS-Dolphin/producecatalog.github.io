b0d5d918817a226ffcff6979a54fbdb0d527dae4
dinhhuyhuy <130577756+dinhhuyhuy@users.noreply.github.com>
2023-12-03 15:05:28 +0700

    update

--- model/productModel.js
+++ model/productModel.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
     name: String,
     price: Number,
+    image: String,
     quantity: Number,
     state: String,
     category: String,
     manufacturer: String,
     creationtime: Date
-}, {collection: 'products'});
+}, {collection: 'product'});
 
-module.exports = mongoose.model('products', productSchema, 'products');
+module.exports = mongoose.model('product', productSchema, 'product');
