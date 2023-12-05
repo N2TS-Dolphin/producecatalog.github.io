5b54300c0f5de05e9356fc1424048188dfa6dfbb
npt5767 <141230403+npt5767@users.noreply.github.com>
2023-12-05 15:30:41 +0700

    filter category

--- routes/user/collection/index.js
+++ routes/user/collection/index.js
@@ -39,17 +39,31 @@ router.get('/', async (req, res, next) => {
     .lean()
     .exec();
 
+  const categories = await Product.distinct('category');
+
   const count =  await Product.countDocuments();
 
+  const selectedCategories = req.query.category;
+  var filteredProducts = products;
+  if (!selectedCategories) {}
+    // Nếu không có danh mục được chọn, hiển thị tất cả sản phẩm
+    else{
+    const temp = await Product
+    .find() // find tất cả các data
+    .lean()
+    .exec();
+      filteredProducts = temp.filter(product => selectedCategories.includes(product.category));
+    }
+
     res.render('user/collection/index', {
-      products: products, // sản phẩm trên một page
+      products: filteredProducts, // sản phẩm trên một page
       pagination: {
         current: page, 
         page,
         pageCount: Math.ceil(count / perPage)
       },
-      
-      layout: 'user/layout.hbs'
+      layout: 'user/layout.hbs',
+      categories
     })
 })
 
